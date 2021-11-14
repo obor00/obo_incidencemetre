c5afbaad5e3b2d2d99f1efb33030cd1949749f39
obor <olivier@obordes.com>
2021-11-14 11:18:30 -0800

    Added mean average, eeprom not working

--- obo_incidencemetre.ino
+++ obo_incidencemetre.ino
@@ -2,6 +2,9 @@
 
   Incidencemetre
 
+  libraries:
+	MPU9250 by hideakitai https://github.com/hideakitai/MPU9250
+	U8G2 by Oliver https://github.com/olikraus/u8g2
 */
 
 #include <Arduino.h>
@@ -15,9 +18,8 @@
 #endif
 
 #include "MPU9250.h"
-
-//#define OLED
-#define _MPU9250
+#include "eeprom_utils.h"
+#include <MovingAverage.h>
 
 /*
   extract from U8g2lib Example Overview:
@@ -27,28 +29,21 @@
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 //#endif
 
-#ifdef _MPU9250
 MPU9250 mpu;
-#endif
+MovingAverage<float> xAvg(10);
+MovingAverage<float> yAvg(10);
+MovingAverage<float> zAvg(10);
 
 void setup(void) {
-#ifdef _MPU9250
-	Serial.begin(115200);
 
-	/* U8g2 Project: SSD1306 Test Board */
-#if 0
-	pinMode(10, OUTPUT);
-	pinMode(9, OUTPUT);
-	digitalWrite(10, 0);
-	digitalWrite(9, 0);
-#endif
-#endif
-//#ifdef OLED
+	//Serial.begin(115200);
+
+	// Oled setting
 	u8g2.begin();
 	u8g2.setBusClock(400000);
 	//u8g2.setFlipMode(0);
-//#endif
-#ifdef _MPU9250
+
+	// MPU9250 setting
 	Wire.begin();
 	delay(2000);
 	if (!mpu.setup(0x68)) {
@@ -57,10 +52,12 @@ void setup(void) {
 			delay(5000);
 		}
 	}
-#endif
+    loadCalibration();
+	mpu.selectFilter(QuatFilterSel::MADGWICK);
+	mpu.setFilterIterations(10);
 }
 
-#ifdef _MPU9250
+
 void print_roll_pitch_yaw() {
 	Serial.print("Yaw, Pitch, Roll: ");
 	Serial.print(mpu.getYaw(), 2);
@@ -69,44 +66,36 @@ void print_roll_pitch_yaw() {
 	Serial.print(", ");
 	Serial.println(mpu.getRoll(), 2);
 }
-#endif
 
 int count = 0;
 
 void write_something() {
-//#ifdef OLED
   u8g2.clearBuffer();					// clear the internal memory
   u8g2.setFont(u8g2_font_ncenB08_tr);	// choose a suitable font
   u8g2.drawStr(0,10,"Incidencemetre");	// write something to the internal memory
 
-  // u8g2.sendBuffer();					// transfer internal memory to the display
-  // String mystr = String(count);
-  // u8g2.drawStr(0,20,mystr.c_str());	// write something to the internal memory
-  // count++;
-  // u8g2.sendBuffer();					// transfer internal memory to the display
+  xAvg.push(mpu.getEulerX());
+  yAvg.push(mpu.getEulerY());
+  zAvg.push(mpu.getEulerZ());
 
   String mystr;
-  mystr = "Yaw = " + String (mpu.getYaw());
+  mystr = "X = " + String (xAvg.get());
   u8g2.drawStr(0,30,mystr.c_str());	// write something to the internal memory
-  mystr = "Pitch = " + String (mpu.getPitch());
+  mystr = "Y = " + String (yAvg.get());
   u8g2.drawStr(0,40,mystr.c_str());	// write something to the internal memory
-  mystr = "Roll = " + String (mpu.getRoll());
+  mystr = "Z = " + String (zAvg.get());
   u8g2.drawStr(0,50,mystr.c_str());	// write something to the internal memory
 
   u8g2.sendBuffer();					// transfer internal memory to the display
-//#endif
 }
 
 void loop(void) {
-  // delay between each page
-#ifdef _MPU9250
   if (mpu.update()) {
 	  static uint32_t prev_ms = millis();
-	  if (millis() > prev_ms + 100) {
+	  if (millis() > prev_ms + 10) {
 		  // print_roll_pitch_yaw();
 		  prev_ms = millis();
 		  write_something();
 	  }
   }
-#endif
 }
